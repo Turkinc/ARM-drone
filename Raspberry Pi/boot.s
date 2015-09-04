923bb7cae98ba6f2ce1acc1da2adb81b3a4285cd
wingsoverglades <wingsoverglades@gmail.com>
2015-09-04 16:55:12 +0000

    added code to turn the ACT LED on, call kmain, then turn it off

--- Raspberry Pi/boot.s
+++ Raspberry Pi/boot.s
@@ -2,7 +2,29 @@
  * Boot file for the Raspberry Pi board
  */
  
- .globl start_
  
+ 
+ 
+ .section .init
+ 
+ .text
+ 
+ .globl start_
  start_:
+ 
+        /* Turn on ACT light */
+        ldr     r0,     =0x20200000
+        mov     r1,     #1
+        lsl     r1,     #18
+        str     r1,     [r0, #4]
         
+        call kmain
+        
+        /* Turn off ACT light */
+        ldr     r0,     =0x20200000
+        mov     r1,     #1
+        lsl     r1,     #18
+        str     r1,     [r0, #4]
+        
+loop$:
+        b       loop$
